992ec6e421d37117213224895e30aa16696e421f
osawa-koki <koki@osawa.dev>
2024-11-29 11:45:59 +0900

    踏み台サーバの定義。

--- lib/compute/compute.ts
+++ lib/compute/compute.ts
@@ -15,6 +15,7 @@ interface ComputeStackProps extends cdk.StackProps {
 
 export default class ComputeStack extends cdk.Stack {
   public readonly fargateService: ecsPatterns.ApplicationLoadBalancedFargateService;
+  public readonly bastionInstance: ec2.Instance;
 
   constructor(scope: Construct, id: string, props: ComputeStackProps) {
     const { stackName, vpc, privateWebSubnets, databaseSecret } = props;
@@ -107,6 +108,20 @@ export default class ComputeStack extends cdk.Stack {
       'Allow only ALB access'
     );
 
+    const bastionInstance = new ec2.Instance(this, 'BastionInstance', {
+      vpc,
+      vpcSubnets: privateWebSubnets,
+      instanceType: new ec2.InstanceType(process.env.BASTION_EC2_INSTANCE_TYPE!),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      role: new iam.Role(this, 'EC2Role', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        ],
+      }),
+    });
+
     this.fargateService = fargateService;
+    this.bastionInstance = bastionInstance;
   }
 }
